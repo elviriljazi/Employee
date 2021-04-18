39f0abde7dbe28e6f337bcc979e9b447a8fb596f
Elvir Iljazi <elvir2k17@outlook.com>
2021-04-18 17:11:42 +0200

    #

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        Employee[] employees = new Employee[10];
-        employees[0] = new Employee();
-        employees[1] = new Employee();
-        employees[0].print();
-        employees[1].print();
-        employees[0].add("Ivana","Ivanova","00011",1,1,1990);
-        employees[1].setName("Petar");
-        employees[1].setSurname("Petrov");
-        employees[1].setUMCN("01010");
-        employees[1].setDate(-1,-1,-1);
+        LinkedList<Employee> employees = new LinkedList<>();
+        employees.add(new Employee());
+        employees.add(new Employee());
+        employees.get(0).print();
+        employees.get(1).print();
+        employees.get(0).add("Ivana","Ivanova","00011",1,1,1990);
+        employees.get(1).setName("Petar");
+        employees.get(1).setSurname("Petrov");
+        employees.get(1).setUMCN("01010");
+        employees.get(1).setDate(-1,-1,-1);
         Employee new1 = new Employee("Stavre","Stavrov","0101010",1,-1,1000);
-        employees[0].print();
-        employees[1].print();
+        employees.get(10).print();
+        employees.get(1).print();
         new1.print();
         new1.print();
 
